3a119d5504e039bfc90ff7672f7948a9bcab2096
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2016-12-23 14:07:14 +0000

    third commit

--- src/ContactImplTest.java
+++ src/ContactImplTest.java
@@ -1,3 +1,5 @@
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,22 +8,35 @@ import static org.junit.Assert.*;
  * Created by Casper on 22/12/2016.
  */
 public class ContactImplTest {
+
+    private Contact testContact;
+
+    @Before
+    public void setupTestContact() {
+        testContact = new ContactImpl(1, "Paul", "myNotes");
+    }
+
     @Test
     public void getId() throws Exception {
+        assertEquals(1, testContact.getId());
 
     }
 
     @Test
+    @Ignore
     public void getName() throws Exception {
+        assertEquals();
 
     }
 
     @Test
+    @Ignore
     public void getNotes() throws Exception {
 
     }
 
     @Test
+    @Ignore
     public void addNotes() throws Exception {
 
     }
