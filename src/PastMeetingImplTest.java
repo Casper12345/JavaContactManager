9154d4c68bec31315166eb1cb288886c99eecf11
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2016-12-31 10:42:28 +0000

    eight commit

---
+++ src/PastMeetingImplTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Casper on 31/12/2016.
+ */
+public class PastMeetingImplTest {
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void getDate() throws Exception {
+
+    }
+
+    @Test
+    public void getContacts() throws Exception {
+
+    }
+
+    @Test
+    public void getNotes() throws Exception {
+
+    }
+
+}
